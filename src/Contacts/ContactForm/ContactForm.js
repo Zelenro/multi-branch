53795a111925303d0054626d51e98132bc14991d
Rostislav <125198729+Zelenro@users.noreply.github.com>
2024-02-15 09:10:29 +0200

    donePhonebookwithoutstyled

---
+++ src/Contacts/ContactForm/ContactForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import {
+  FormButton,
+  FormContainer,
+  FormInput,
+  FormLabel,
+} from './ContactForm.styled';
+import { initialValues, validationSchema } from './ContactFormSchema';
+import { FormError } from './ContactFormError';
+
+const ContactForm = ({ handlerSubmit }) => {
+  return (
+    <>
+      <FormContainer>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handlerSubmit}
+        >
+          <Form autoComplete="off">
+            <FormLabel htmlFor="name">
+              Name
+              <FormInput
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                required
+              />
+              <FormError name="name" />
+            </FormLabel>
+            <FormLabel htmlFor="number">
+              Number
+              <FormInput
+                type="text"
+                id="number"
+                name="number"
+                placeholder="Number"
+                required
+              />
+              <FormError name="number" />
+            </FormLabel>
+            <FormButton type="submit">Add contact</FormButton>
+          </Form>
+        </Formik>
+      </FormContainer>
+    </>
+  );
+};
+
+export default ContactForm;
