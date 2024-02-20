a036f42a1d131950af928215d52cd14c0e5af132
Rostislav <125198729+Zelenro@users.noreply.github.com>
2024-02-20 19:58:16 +0200

    debug

--- src/ImageFinder/ImageFinder.js
+++ src/ImageFinder/ImageFinder.js
@@ -3,56 +3,45 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import * as API from './Services/api';
+import { Circles } from 'react-loader-spinner';
 
 class ImageFinder extends Component {
   state = {
     images: [],
     isLoading: false,
+    error: false,
     page: '1',
     perPage: '12',
-    searchImages: '',
-  };
-
-  // handlerInput = e => {
-  //   const { value } = e.currentTarget;
-  //   this.setState({ searchImages: value });
-  //   return value;
-  // };
-
-  // handlerInput = e => {
-  //   const { name, value } = e.currentTarget;
-  //   this.setState(prevState => ({
-  //     ...prevState,
-  //     [name]: value,
-  //   }));
-  //   return value;
-  // };
-
-  handleChange = value => {
-    console.log(value);
-    this.setState(prevState => ({
-      ...prevState,
-      // [name]: value,
-    }));
+    searchImages: null,
+    imagesLoaded: false,
   };
 
   findImage = async value => {
-    console.log(value);
-    const images = await API.getImages(value);
-    console.log(images);
-    this.setState({ images: images });
+    try {
+      this.setState({ isLoading: true });
+      this.setState({ searchImages: value });
+      const { searchImages } = this.state;
+      const arrayImg = await API.getImages(searchImages);
+      this.setState({ images: arrayImg });
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    } finally {
+      this.setState({ isLoading: false, imagesLoaded: true });
+    }
   };
 
-  componentDidMount() {
-    if (!this.state.searchImages) {
-      this.findImage(this.state.searchImages);
+  async componentDidMount() {
+    try {
+      await this.findImage();
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
     }
   }
 
   componentDidUpdate(_, prevState) {
-    if (this.state.searchImages === null) {
-      console.log(this.state.searchImages);
-      // localStorage.setItem('images', JSON.stringify(this.state.images));
+    if (this.state.searchImages !== prevState.searchImages) {
       this.findImage(this.state.searchImages);
     }
   }
@@ -60,10 +49,29 @@ class ImageFinder extends Component {
   componentWillUnmount() {}
 
   render() {
+    const { images, isLoading, error, searchImages, imagesLoaded } = this.state;
     return (
       <>
+        {error && <p>Что пошло не так !!!</p>}
         <Searchbar onSubmit={this.findImage} handleChange={this.handleChange} />
-        <ImageGallery images={this.state.images} />
+        {isLoading || !imagesLoaded ? (
+          <Circles
+            height="80"
+            width="80"
+            color="#3f51b5"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        ) : (
+          <ImageGallery
+            images={images}
+            error={error}
+            searchImages={searchImages}
+          />
+        )}
+
         <Button />
       </>
     );
@@ -71,6 +79,29 @@ class ImageFinder extends Component {
 }
 
 export default ImageFinder;
+
+// handlerInput = e => {
+//   const { value } = e.currentTarget;
+//   this.setState({ searchImages: value });
+//   return value;
+// };
+
+// handlerInput = e => {
+//   const { name, value } = e.currentTarget;
+//   this.setState(prevState => ({
+//     ...prevState,
+//     [name]: value,
+//   }));
+//   return value;
+// };
+
+// handleChange = value => {
+//   // console.log(value);
+//   this.setState(prevState => ({
+//     ...prevState,
+//     // [name]: value,
+//   }));
+// };
 
 // const { searchImages, page, perPage } = this.state;
 // const images = await API.getImages(searchImages, page, perPage);
--- src/ImageFinder/Searchbar/Searchbar.js
+++ src/ImageFinder/Searchbar/Searchbar.js
@@ -1,41 +1,38 @@
 import { Field, Form, Formik } from 'formik';
 
-export const Searchbar = ({ onSubmit, onChangeText }) => {
-  const handleSubmit = (values, actions) => {
-    onSubmit(values);
-
+export const Searchbar = ({ onSubmit }) => {
+  const handleSubmit = async (values, actions) => {
+    await onSubmit(values.searchImages);
+    actions.setSubmitting(false);
+    // console.log(values);
     actions.resetForm();
-  };
-
-  const handleChange = value => {
-    // console.log(value.nativeEvent.data);
-    // return value.nativeEvent.data;
-
-    // const { name, value } = event.target; // Destructure name and value from the event target
-    console.log(value);
-    // onChangeText({ [name]: value }); // Notify parent component about the change
   };
 
   return (
     <header className="Searchbar">
       <Formik initialValues={{ searchImages: '' }} onSubmit={handleSubmit}>
-        <Form className="SearchForm">
-          <button type="submit" className="SearchForm-button">
-            <span className="button-label">Search</span>
-          </button>
+        {props => {
+          // console.log(props);
+          return (
+            <Form className="SearchForm">
+              <button
+                type="submit"
+                className="SearchForm-button"
+                disabled={props.isSubmitting}
+              >
+                <span className="button-label">Search</span>
+              </button>
 
-          <Field
-            className="SearchForm-input"
-            type="text"
-            name="searchImages"
-            // value={value}
-            //   autocomplete="off"
-            //   autofocus
-            // onChange={handlerInput}
-            onChange={handleChange}
-            placeholder="Search images and photos"
-          />
-        </Form>
+              <Field
+                className="SearchForm-input"
+                type="text"
+                name="searchImages"
+                onChange={props.handleChange}
+                placeholder="Search images and photos"
+              />
+            </Form>
+          );
+        }}
       </Formik>
     </header>
   );
